a47e844fad3d249f79a88bc2ec8c4c162670a472
Juliette Michaux <miamiame@hotmail.com>
2019-03-20 15:52:43 +0100

    atelierwilders

---
+++ wilders.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Alexandre'));
+console.log(chalk.red('Tanguy'));
+console.log(chalk.green('Romain'));
+console.log(chalk.yellow('Florin'));
